16ddec0cc3e9dd42bccf63029086d3d10e1563ec
Nimish Dadlani <nmd2611@gmail.com>
2022-01-20 13:12:00 +0530

    Web3 dependency added & folder structure modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
 import "./App.css";
+import { Home } from "./pages";
 
 function App() {
   return (
--- src/components/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Home.module.css";
-
-const Home = () => {
-  return <div className={styles.root}>Voting App - Client</div>;
-};
-
-export default Home;
