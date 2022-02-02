cfe5345b1fc98b04676cb15b3c7b50868cfb4961
Nimish Dadlani <nmd2611@gmail.com>
2022-02-02 19:01:10 +0530

    Chakra UI added

---
+++ src/utils/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+// setting the color pallette
+export const theme = extendTheme({
+  colors: {
+    primary: "#0085FF",
+    secondary: "",
+  },
+});
