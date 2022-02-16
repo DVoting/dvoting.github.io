84ada62936f3541b465b2d4bc17acdc1018b46ea
Sanchitraina1999 <sanchitraina1999@gmail.com>
2022-02-16 20:40:22 +0530

    changePassword done

--- src/services/otpActions.js
+++ src/services/otpActions.js
@@ -28,17 +28,3 @@ export const verifyOTP = async (emailAndOtp) => {
         throw error;
     }
 };
-
-export const updatePasword = async (emailAndPassword) => {
-    try {
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        const res = await axios.put(`${BASE_URL}/users/profile`, emailAndPassword, config);
-        return res.data;
-    } catch (error) {
-        throw error;
-    }
-};
