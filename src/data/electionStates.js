a7a1b02de23f49c29190808688bb398dcbd19698
HeptaDecane <sooraj1999vs@gmail.com>
2022-03-11 16:06:41 +0530

    fix election states

--- src/data/electionStates.js
+++
@@ -1,8 +0,0 @@
-const electionStates = [
-    { name: 'Not applied for election', value: '1' },
-    { name: 'Wallet not signed', value: '2' },
-    { name: 'Not voted in election', value: '3' },
-    { name: 'Appeared in election', value: '4' }
-];
-
-export default electionStates;
