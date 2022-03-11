1120ef90fc6e37ff0bc278e4c3a7324d43e9dbb1
Sanchitraina1999 <sanchitraina1999@gmail.com>
2022-03-11 12:04:24 +0530

    explore Elections

--- src/pages/ElectionDetails.jsx
+++ src/pages/ElectionDetails.jsx
@@ -1,19 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { FormContainer, Loader } from "../containers";
+import { GlobalContext } from "../context/GlobalContext";
+import { getElectionDetails } from "../services/electionActions";
 
 const ElectionDetails = () => {
+  const { loading, setLoading } = useContext(GlobalContext);
   const location = useLocation();
 
   let electionId = location.pathname.split("/")[2];
+  const [election, setElection] = useState({})
 
-  console.log(electionId);
+  useEffect(async () => {
+    setLoading(true);
+    try {
+      setElection(await getElectionDetails(electionId));
+    }
+    catch (err) {
+      console.log(err)
+    }
+    setLoading(false);
+  }, [])
 
   return (
-    <div>
-      <h1>Election Details</h1>
-      {electionId}
-      {/* <h1>{electionId}</h1> */}
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h1>Election Details</h1>
+          Election Title: {election.title}
+          <br />
+          OpenTimeStamp: {election.openTimestamp}
+          <br />
+          Election CloseTimeStamp: {election.closeTimestamp}
+          <br />
+          <a className="btn btn-primary" href={election.registrationLink}>Apply</a>
+          <br />
+        </div>
+      )}
+    </>
   );
 };
 
