4f1e01b7240204799927a12d0f665ce7be98ae47
Sanchitraina1999 <sanchitraina1999@gmail.com>
2022-03-16 14:44:13 +0530

    explore + changes in voting page

---
+++ src/components/Election.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Election = ({ election }) => {
+    return (
+        <Card className='m-2 py-2 rounded btn-outline-dark' style={{ width: '16rem' }}>
+            <Card.Body>
+                <Card.Title>
+                    <a href={`/elections/${election._id}`}>{election.title}</a>
+                </Card.Title>
+                <Card className='py-2 rounded'>
+                    <Card.Text style={{ color: 'green' }}>
+                        Opening Time:<br />
+                        {new Date(election.openTimestamp).toString()}
+                    </Card.Text>
+                    <Card.Text style={{ color: 'red' }}>
+                        Closing Time:<br />
+                        {new Date(election.closeTimestamp).toString()}
+                    </Card.Text>
+                </Card>
+            </Card.Body>
+        </Card>
+    )
+};
+
+export default Election;
