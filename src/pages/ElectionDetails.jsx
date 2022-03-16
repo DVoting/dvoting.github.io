c5e081c2487e906493a610c4c7b76e7f02385e5a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2022-03-16 16:32:50 +0530

    conditions on voting phase

--- src/pages/ElectionDetails.jsx
+++ src/pages/ElectionDetails.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import {Navigate, useLocation} from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { Loader } from "../containers";
 import { GlobalContext } from "../context/GlobalContext";
 import { getElectionDetails } from "../services/electionActions";
-import {approveElection, getVoterById} from "../services/voterServices";
-import {Button} from "react-bootstrap";
-import {toast} from "react-toastify";
+import { approveElection, getVoterById } from "../services/voterServices";
+import { Button } from "react-bootstrap";
+import { toast } from "react-toastify";
 import Message from "../utils/Message";
 import web3 from "../utils/web3";
-import {signMessage} from "../utils/wallet";
+import { signMessage } from "../utils/wallet";
 
 const ElectionDetails = () => {
   const { isAuth, loading, setLoading, user } = useContext(GlobalContext);
@@ -21,37 +21,39 @@ const ElectionDetails = () => {
   const [voter, setVoter] = useState(null)
   const [electionState, setElectionState] = useState(0)
 
-  React.useEffect(async ()=>{
+  let currentTime = new Date().toISOString();
+
+  React.useEffect(async () => {
     setLoading(true);
 
-    try{
+    try {
       setElection(await getElectionDetails(electionId));
-      if(user)
+      if (user)
         setVoter(await getVoterById(user.uniqueVoterId));
     }
     catch (err) {
       console.log(err)
     }
     setLoading(false);
-  },[user])
+  }, [user])
 
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     if (user) {
       // already voted
-      if(election?.appearedVoters?.includes(user.uniqueVoterId))
+      if (election?.appearedVoters?.includes(user.uniqueVoterId))
         setElectionState(5)
 
       // vote
-      else if(voter?.signedElections?.includes(electionId))
+      else if (voter?.signedElections?.includes(electionId))
         setElectionState(4)
 
       // sign wallet
-      else if(election?.approvedVoters?.includes(user.uniqueVoterId))
+      else if (election?.approvedVoters?.includes(user.uniqueVoterId))
         setElectionState(3)
 
       // wait for approval
-      else if(election?.appliedVoters?.includes(user.uniqueVoterId))
+      else if (election?.appliedVoters?.includes(user.uniqueVoterId))
         setElectionState(2)
 
       // apply
@@ -60,7 +62,7 @@ const ElectionDetails = () => {
     }
   }, [user, voter, election])
 
-  const signElection = async ()=>{
+  const signElection = async () => {
     if (!walletId) {
       toast.error(Message('No wallet connected', null), {
         position: toast.POSITION.BOTTOM_RIGHT,
@@ -79,7 +81,7 @@ const ElectionDetails = () => {
     })
 
     let signature = await signMessage(message)
-    if(!signature) return
+    if (!signature) return
 
     let data = {
       address: web3.utils.toChecksumAddress(walletId),
@@ -87,7 +89,7 @@ const ElectionDetails = () => {
     }
 
     const res = await approveElection(electionId, data)
-    if(res.error){
+    if (res.error) {
       console.log(res)
       toast.error(Message(`error ${res.status}`, `${res.data || res.statusText}`), {
         position: toast.POSITION.BOTTOM_RIGHT,
@@ -101,8 +103,8 @@ const ElectionDetails = () => {
     setRedirect('/dashboard')
   }
 
-  if(redirect) return <Navigate replace to={redirect} />
-  if(loading) return <Loader/>
+  if (redirect) return <Navigate replace to={redirect} />
+  if (loading) return <Loader />
 
   return (
     <React.Fragment>
@@ -113,39 +115,46 @@ const ElectionDetails = () => {
       <br />
       Election CloseTimeStamp: {election.closeTimestamp}
       <div>
-      {isAuth &&
-        <React.Fragment>
-          {electionState===1 &&
-            <a href={`${election.registrationLink}`} className="btn btn-primary">
-              Apply
-            </a>
-          }
+        {isAuth &&
+          <React.Fragment>
+            {electionState === 1 &&
+              <a href={`${election.registrationLink}`} className="btn btn-primary">
+                Apply
+              </a>
+            }
 
-          {electionState===2 &&
-            <button disabled={true} className="btn btn-primary">
-              Wait for approval
-            </button>
-          }
+            {electionState === 2 &&
+              <button disabled={true} className="btn btn-primary">
+                Wait for approval
+              </button>
+            }
 
-          {electionState===3 &&
-            <Button variant='primary' onClick={async()=>{await signElection()}}>
-              Sign Wallet
-            </Button>
-          }
+            {electionState === 3 &&
+              <Button variant='primary' onClick={async () => { await signElection() }}>
+                Sign Wallet
+              </Button>
+            }
 
-          {electionState===4 &&
-            <a href={`/elections/${electionId}/vote`} className="btn btn-primary">
-              VOTE
-            </a>
-          }
+            {electionState === 4 &&
+              <>
+                <Button href={`/elections/${electionId}/vote`} className="btn btn-primary" disabled={
+                  (currentTime > election.openTimestamp && currentTime > election.closeTimestamp) ||
+                  (currentTime < election.openTimestamp && currentTime < election.closeTimestamp)
+                }>
+                  VOTE
+                </Button>
+                {(currentTime > election.openTimestamp && currentTime > election.closeTimestamp) && <div style={{ color: 'red' }}>Election has finished</div>}
+                {(currentTime < election.openTimestamp && currentTime < election.closeTimestamp) && <div style={{ color: 'red' }}>Election has not started yet</div>}
+              </>
+            }
 
-          {electionState===5 &&
-            <button className="btn btn-primary" disabled={true}>
-              Already Voted!
-            </button>
-          }
-        </React.Fragment>
-      }
+            {electionState === 5 &&
+              <button className="btn btn-primary" disabled={true}>
+                Already Voted!
+              </button>
+            }
+          </React.Fragment>
+        }
       </div>
     </React.Fragment>
   );
