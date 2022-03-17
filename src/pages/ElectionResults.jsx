3fa514682f66e6b36949bfd56d71b9860a8f3cd6
Nimish Dadlani <nmd2611@gmail.com>
2022-03-17 10:59:59 +0530

    Error message added

--- src/pages/ElectionResults.jsx
+++ src/pages/ElectionResults.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { getElectionDetails } from "../services/electionActions";
 
 const ElectionResults = () => {
+  const [election, setElection] = useState({});
   const location = useLocation();
 
   let electionId = location.pathname.split("/")[2];
 
-  console.log(electionId);
+  useEffect(() => {
+    (async () => {
+      const res = await getElectionDetails(electionId);
+
+      if (res.error) return;
+
+      setElection(res);
+    })();
+  }, []);
+
+  console.log(election);
+
+  let hasBeenDeployed = election?.deploymentAddress;
+  let hasStarted = new Date(election?.openTimestamp) <= new Date();
+  let hasEnded = new Date(election?.closeTimestamp) < new Date();
+
+  // console.log(hasStarted, hasEnded);
+
+  if (!hasBeenDeployed || !hasStarted) {
+    return (
+      <div>
+        <h2>Election Result Page</h2>
+        Election hasn't started yet. Please check after sometime.
+      </div>
+    );
+  } else if (!hasEnded) {
+    return (
+      <div>
+        <h2>Election Result Page</h2>
+        Election is in progress. Please check after sometime.
+      </div>
+    );
+  }
 
   return <div>Election Result Page</div>;
 };
