bf6afc4e2e74102de730219763651a80f4cfd14b
Nimish Dadlani <nmd2611@gmail.com>
2022-03-17 14:12:08 +0530

    REsults almost done

--- src/pages/ElectionResults.jsx
+++ src/pages/ElectionResults.jsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getElectionDetails } from "../services/electionActions";
+import Table from "react-bootstrap/Table";
+import { PieChart } from "react-minimal-pie-chart";
+
+import { transformDataForPie } from "../utils";
+import ElectionContract from "../contracts/Election.js";
+
+const getElectionResultFromContract = async (address) => {
+  const election = ElectionContract(address);
+
+  const status = await election.methods.getCurrentStatus().call();
+
+  return status[0];
+};
 
 const ElectionResults = () => {
   const [election, setElection] = useState({});
+  const [results, setResults] = useState([]);
+
   const location = useLocation();
 
   let electionId = location.pathname.split("/")[2];
@@ -18,13 +33,22 @@ const ElectionResults = () => {
     })();
   }, []);
 
-  console.log(election);
-
   let hasBeenDeployed = election?.deploymentAddress;
   let hasStarted = new Date(election?.openTimestamp) <= new Date();
   let hasEnded = new Date(election?.closeTimestamp) < new Date();
 
-  // console.log(hasStarted, hasEnded);
+  useEffect(() => {
+    (async () => {
+      if (hasEnded && election?.deploymentAddress) {
+        const res = await getElectionResultFromContract(
+          election.deploymentAddress
+        );
+
+        setResults(transformDataForPie(res));
+        console.log(res);
+      }
+    })();
+  }, [election]);
 
   if (!hasBeenDeployed || !hasStarted) {
     return (
@@ -42,7 +66,63 @@ const ElectionResults = () => {
     );
   }
 
-  return <div>Election Result Page</div>;
+  return (
+    <div>
+      <h2>Election Result Page</h2>
+      <p>Election Id: {electionId}</p>
+      <p>Election Name: {election.title}</p>
+      <div className='d-flex flex-column align-items-center'>
+        <div className='py-5' style={{ maxWidth: "60%" }}>
+          <PieChart
+            data={results}
+            label={(props) => {
+              const { dataEntry } = props;
+              console.log(props);
+
+              // return `
+              //     ${dataEntry.title}
+              //     ${Math.round(dataEntry.percentage)}%
+              //   `;
+              return `${Math.round(dataEntry.percentage)} %`;
+            }}
+            labelStyle={(index) => ({
+              fontSize: "5px",
+              textAlign: "center",
+              cursor: "pointer",
+            })}
+            startAngle={-90}
+            // segmentsShift={(index) => (index === selected ? 2 : 0)}
+            // onClick={(event, index) => {
+            //   setSelected(index === selected ? undefined : index);
+            // }}
+            animate
+          />
+        </div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Candidate Name</th>
+              <th>Votes Received</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results?.map((item) => {
+              const { id, title, value } = item;
+
+              return (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                  <td>{value}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default ElectionResults;
